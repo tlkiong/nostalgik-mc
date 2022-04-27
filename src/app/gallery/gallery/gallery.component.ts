43bbf7ee90bc343922421a29a334e748325ccb05
Kiong <kiong90@gmail.com>
2022-04-27 08:49:54 +0800

    feat: gallery base style done

--- src/app/gallery/gallery/gallery.component.ts
+++ src/app/gallery/gallery/gallery.component.ts
@@ -9,7 +9,28 @@ export class GalleryComponent implements OnInit {
   galleryImages: {
     path: string;
     caption: string;
-  }[] = [];
+  }[] = [
+    {
+      caption: 'AlterDimension',
+      path: 'AlterDimension.png',
+    },
+    {
+      caption: 'CCStelleri',
+      path: 'CCStelleri.png',
+    },
+    {
+      caption: 'Mozo95',
+      path: 'Mozo95.png',
+    },
+    {
+      caption: 'TheTrulyMad',
+      path: 'TheTrulyMad.png',
+    },
+    {
+      caption: 'Vinaco',
+      path: 'Vinaco.png',
+    },
+  ];
   constructor() {}
 
   ngOnInit(): void {
